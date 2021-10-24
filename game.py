f4a32cefa501a518303c02db40ba11e7b5c23f2d
Monijo <mokajozefowicz@gmail.com>
2021-10-24 15:12:10 +0200

    checking what dice inside

--- game.py
+++ game.py
@@ -5,7 +5,9 @@ types_dices = ["D3", "D4", "D6", "D8", "D10", "D12", "D20", "D100"]
 
 def dice(dice_code):
     dice_code = dice_code
-    
+    for dice in types_dices:
+        if dice in dice_code:
+            print(dice)
 
 print(dice("2D10+8"))
 
