c3ff88a214a4c7b01cce7bb702a68c4cc4024de3
Monijo <mokajozefowicz@gmail.com>
2021-10-25 10:23:23 +0200

    wrong input

--- game.py
+++ game.py
@@ -8,11 +8,13 @@ def dice(dice_code):
     for dice in types_dices:
         if dice in dice_code:
             try:
-                list_dice_code = dice_code.split(dice)
+                multiply, modifier = dice_code.split(dice)
                 actual_dice = dice
             except ValueError:
                 return "Wrong input"
             break
+    else:
+        return "Wrong input"
 
     throws = int(list_dice_code[0])
     extra_value = list_dice_code[1]
